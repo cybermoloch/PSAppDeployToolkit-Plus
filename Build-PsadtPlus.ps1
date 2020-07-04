262c7b724ef605c359d03978c50b343b1bfd91e2
CyberMoloch <4962269-cybermoloch@users.noreply.gitlab.com>
2020-07-04 00:01:36 -0600

    Add output message to CI and fix SHORT_SHA var

--- Build-PsadtPlus.ps1
+++ Build-PsadtPlus.ps1
@@ -6,9 +6,13 @@ $NtfsSecurityUri = 'https://github.com/raandree/NTFSSecurity/releases/download/4
 $VcRedistUri = 'https://github.com/aaronparker/VcRedist/archive/v2.0.163.zip'
 $SetUserFtaUri = 'https://kolbi.cz/SetUserFTA.zip'
 
+Write-Output 'Downloading upstream PSAppDeployToolkit...'
 Invoke-WebRequest -Uri $PsadtUri -OutFile 'PSAppDeployToolkit.zip' -UseBasicParsing
+Write-Output 'Downloading upstream NTFSSecurity Module...'
 Invoke-WebRequest -Uri $NtfsSecurityUri -OutFile 'NTFSSecurity.zip'-UseBasicParsing
+Write-Output 'Downloading upstream VcRedist Module...'
 Invoke-WebRequest -Uri $VcRedistUri -OutFile 'VcRedist.zip' -UseBasicParsing
+Write-Output 'Downloading upstream SetUserFTA...'
 Invoke-WebRequest -Uri $SetUserFtaUri -OutFile 'SetUserFTA.zip' -UseBasicParsing
 
 Unblock-File -Path '*.zip'
@@ -17,35 +21,43 @@ $toolkitPath = 'PSADTPlus\AppDeployToolkit'
 $modulesPath = 'PSADTPlus\AppDeployToolkit\Modules'
 $extrasPath = 'PSADTPlus\AppDeployToolkit\Extras'
 
-@($modulesPath, $extrasPath) | ForEach-Object -Process { New-Item -Path $PSItem -ItemType Directory -Force }
+Write-Output ('Creating directions: ' + $modulesPath + ' and ' + $extrasPath)
+@($modulesPath, $extrasPath) | ForEach-Object -Process { New-Item -Path $PSItem -ItemType Directory -Force  | Out-Null }
 
+Write-Output 'Extracting archives...'
 Get-ChildItem -Path '*.zip' | ForEach-Object -Process { Expand-Archive -Path $PSItem -Force }
 
+Write-Output 'Copying upstream PSApplDeployToolkit files...'
 Copy-Item -Path 'PSAppDeployToolkit\Toolkit\*' -Destination 'PSADTPlus' -Recurse -Force
 $PsadtLicenseUri = 'https://raw.githubusercontent.com/PSAppDeployToolkit/PSAppDeployToolkit/master/LICENSE'
 Invoke-WebRequest -Uri $PsadtLicenseUri -OutFile 'PSADTPlus\LICENSE'
 
+Write-Output 'Copying NTFSSecurity Module files...'
 Copy-Item -Path 'NTFSSecurity' -Destination $modulesPath -Recurse -Force
 $NtfsSecurityLicenseUri = 'https://raw.githubusercontent.com/raandree/NTFSSecurity/master/LICENSE'
 Invoke-WebRequest -Uri $NtfsSecurityLicenseUri -OutFile ($modulesPath + '\NTFSSecurity\LICENSE')
 
+Write-Output 'Copying VcRedist Module files...'
 $vcRedistSrcPath = Get-ChildItem -Path 'VcRedist' -Directory | Get-ChildItem -Filter 'VcRedist' -Directory
 Copy-Item -Path $vcRedistSrcPath.FullName -Destination ($modulesPath + '\VcRedist') -Recurse -Force
 $VcRedistLicenseUri = 'https://raw.githubusercontent.com/aaronparker/VcRedist/master/LICENSE'
 Invoke-WebRequest -Uri $VcRedistLicenseUri -OutFile ($modulesPath + '\VcRedist\LICENSE')
 
+Write-Output 'Copying SetUserFTA files...'
 Copy-Item -Path 'SetUserFTA\SetUserFTA\SetUserFTA.exe' -Destination ($extrasPath + '\SetUserFTA.exe') -Force
 Copy-Item -Path 'SetUserFTA\SetUserFTA\EULA.txt' -Destination ($extrasPath + '\SetUserFTA-EULA.txt') -Force
 
+Write-Output 'Copying PSAppDeployToolkit-Plus files...'
 Copy-Item -Path 'Toolkit\Deploy-Application.ps1' -Destination 'PSADTPlus\Deploy-Application.ps1' -Force
 Copy-Item -Path 'Toolkit\AppDeployToolkit\AppDeployToolkitExtensions.ps1' -Destination ($toolkitPath + '\AppDeployToolkitExtensions.ps1') -Force
 Copy-Item -Path 'Toolkit\AppDeployToolkit\AppDeployToolkitBanner.png' -Destination ($toolkitPath + '\AppDeployToolkitBanner.png') -Force
 Copy-Item -Path 'Toolkit\AppDeployToolkit\AppDeployToolkitLogo.ico' -Destination ($toolkitPath + '\AppDeployToolkitLogo.ico') -Force
 
 If (Test-Path -Path Env:\CI_COMMIT_SHORT_SHA) {
-    $PsadtPlusFilename = ('PSADTPlus-' + ${env:CI_COMMIT)CI_COMMIT_SHORT_SHA} + '.zip')
+    $PsadtPlusFilename = ('PSADTPlus-' + ${env:CI_COMMIT_SHORT_SHA} + '.zip')
 } else {
     $PsadtPlusFilename = 'PSADTPlus.zip'
 }
 
+Write-Output ('Building zip package: ' + $PsadtPlusFilename)
 Compress-Archive -Path 'PSADTPlus\*' -DestinationPath $PsadtPlusFilename -Force
